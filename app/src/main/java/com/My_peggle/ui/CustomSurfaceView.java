d7914e8c6f197b36c905488f7bb0251bc8b8e7ec
ohayon02 <s09719@haklaiph.org>
2026-01-04 09:47:31 +0200

    deleted unnecessary shapes, added mainactivity

--- app/src/main/java/com/My_peggle/ui/CustomSurfaceView.java
+++ app/src/main/java/com/My_peggle/ui/CustomSurfaceView.java
@@ -11,11 +11,7 @@ import android.view.SurfaceView;
 
 import com.My_peggle.R;
 import com.My_peggle.shapes.BaseShape;
-import com.My_peggle.shapes.CircleShape;
 import com.My_peggle.shapes.ImageShape;
-import com.My_peggle.shapes.RectShape;
-import com.My_peggle.shapes.TeleportCircle;
-import com.My_peggle.shapes.TriangleShape;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +20,6 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
     private GameThread gameThread;
     private final List<BaseShape> shapes = new ArrayList<>();
     private BaseShape selectedShape = null;
-    private TeleportCircle specialCircle;
     private boolean shapesInitialized = false;
     private Bitmap backgroundBitmap;
 
@@ -39,21 +34,6 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
         if (originalBackground != null) {
             backgroundBitmap = Bitmap.createScaledBitmap(originalBackground, viewWidth, viewHeight, true);
         }
-
-        // 3 Rectangles
-        shapes.add(new RectShape(200, 200, 200, 150, Color.RED));
-        shapes.add(new RectShape(500, 200, 150, 150, Color.BLUE));
-        shapes.add(new RectShape(800, 200, 100, 250, Color.GREEN));
-
-        // 1 Normal Circle
-        shapes.add(new CircleShape(300, 600, 80, Color.MAGENTA));
-
-        // 1 Special Teleport Circle
-        specialCircle = new TeleportCircle(700, 600, 100, Color.CYAN, Color.WHITE);
-        shapes.add(specialCircle);
-
-        // 1 Triangle
-        shapes.add(new TriangleShape(500, 1000, 150, Color.YELLOW));
 
         float containerWidth = 1200f;
         // Stretch the height to be 20% taller than the screen
@@ -120,26 +100,13 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                // Logic 1: Teleportation if active
-                if (specialCircle.isActive) {
-                    specialCircle.setPosition(x, y);
-                    specialCircle.setActive(false);
-                    return true;
-                }
-
-                // Logic 2: Hit detection
                 for (int i = shapes.size() - 1; i >= 0; i--) {
                     BaseShape shape = shapes.get(i);
                     if (shape.isTouched(x, y)) {
-                        if (shape == specialCircle) {
-                            specialCircle.setActive(true);
-                        } else {
+                        if (shape.isMovable()) {
                             selectedShape = shape;
-                            // Bring to front
-                            shapes.remove(i);
-                            shapes.add(shape);
+                            break;
                         }
-                        return true;
                     }
                 }
                 break;
